09bd3034b5135bb02a45a9e5e30ef4b9756d6595
Bohdan Bakun <hasps.bowler-0d@icloud.com>
2024-02-27 20:46:02 +0200

    Vievs added: ListView, ListRowView, AddView

---
+++ ToDoListSwiftUI/Views/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  ToDoListSwiftUI
+//
+//  Created by Богдан Бакун on 27.02.2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
