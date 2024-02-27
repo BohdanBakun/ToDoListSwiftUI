226a1ef755c1a72a55657608ad85c28e815d0eb7
Bohdan Bakun <hasps.bowler-0d@icloud.com>
2024-02-27 17:52:53 +0200

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Богдан Бакун on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
