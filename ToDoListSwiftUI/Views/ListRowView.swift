3f9ed146fae9a40af2230b96a30fe726e85a243e
Bohdan Bakun <hasps.bowler-0d@icloud.com>
2024-02-28 14:02:47 +0200

    Model added

--- ToDoListSwiftUI/Views/ListRowView.swift
+++ ToDoListSwiftUI/Views/ListRowView.swift
@@ -8,17 +8,22 @@
 import SwiftUI
 
 struct ListRowView: View {
-    let title: String
+    
+    let item: ItemModel
     
     var body: some View {
         HStack {
-            Image(systemName: "checkmark.circle")
-            Text(title)
+            Image(systemName: item.isCompleted ?  "checkmark.circle" : "circle")
+                .foregroundStyle(item.isCompleted ? .green : .yellow)
+            Text(item.title)
             Spacer()
         }
+        .font(.title2)
+        .padding(.vertical, 8)
     }
 }
 
 #Preview {
-    ListRowView(title: "This is the first title")
+    let item1 = ItemModel(title: "First", isCompleted: false)
+    return ListRowView(item: item1)
 }
