bc7050a07399d2f2ff6130a6465aa5f58948240f
Bohdan Bakun <hasps.bowler-0d@icloud.com>
2024-02-28 21:44:57 +0200

    NoitemsView added. Colors for dark/light mods added.

---
+++ ToDoListSwiftUI/Views/NoItemsViews.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsViews.swift
+//  ToDoListSwiftUI
+//
+//  Created by Богдан Бакун on 28.02.2024.
+//
+
+import SwiftUI
+
+struct NoItemsViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsViews()
+}
